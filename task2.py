128fbb971c79a36291d3de5f2c151977d61d50fb
nigorshkov <61785056+nigorshkov@users.noreply.github.com>
2020-03-09 15:04:56 +0300

    Change task2

--- task2.py
+++ task2.py
@@ -3,11 +3,4 @@ hours: int = input_time // 3600
 minutes = (input_time // 60) % 60
 seconds = input_time % 60
 
-if hours < 10:
-    hours = str('0' + str(hours))
-if minutes < 10:
-    minutes = str('0' + str(minutes))
-if seconds < 10:
-    seconds = str('0' + str(seconds))
-
-print(hours, ":", minutes, ":", seconds)
+print(f'{hours:>02}:{minutes:>02}:{seconds:>02}')
