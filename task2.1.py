6ffd811a6de6920e89c76a61558f100af3da2453
nigorshkov <61785056+nigorshkov@users.noreply.github.com>
2020-03-12 20:44:30 +0300

    Without tasks 2

--- task2.1.py
+++
@@ -1,4 +0,0 @@
-list_el = [1, 1.23, complex(4, 7), True, 'hello', [1, '2'], (1,2), {1, 3}]
-
-for i in list_el:
-    print(type(i))
--- task2.4.py
+++
@@ -1,6 +0,0 @@
-list_str = input('Введите строку: ').split(' ')
-for ind, i in enumerate(list_str):
-    if len(i) > 10:
-        print(f"{ind+1} {i [0:10]}")
-    else:
-        print(ind+1, i)
