db72a2d1f573780f01337f0e612117af00e3539e
nigorshkov <61785056+nigorshkov@users.noreply.github.com>
2020-03-13 21:13:57 +0300

    Without tasks3

--- task3.2.py
+++
@@ -1,8 +0,0 @@
-def info(name, surname, year, city, email, phone):
-    print(
-        f"имя - {name}, фамилия - {surname}, год  рождения  - {year}, город проживания - {city}, email - {email}, "
-        f"телефон - {phone}")
-
-
-info(name=input('Введите имя: '), surname=input('Введите фамилия: '), year=input('Введите год  рождения: '),
-     city=input('Введите город проживания: '), email=input('Введите email: '), phone=input('Введите телефон: '))
--- task3.6.py
+++
@@ -1,8 +0,0 @@
-def int_func(arg):
-    return arg.capitalize()
-
-
-str_up = input('Введите строку:')
-a = str_up.split(' ')
-for i in a:
-    print(int_func(i), end=' ')
